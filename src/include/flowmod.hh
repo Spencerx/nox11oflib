dc23f813bd96a70db3b5462a3d6185145816de66
eder <eder@cpqd.com.br>
2012-05-16 13:08:05 -0300

    Class FlowMod created to ease flow modifications messages creation.

---
+++ src/include/flowmod.hh
@@ -0,0 +1,33 @@
+#ifndef FLOWMOD_HH
+#define FLOWMOD_HH 1
+
+#include "flow.hh"
+#include "../oflib/ofl-messages.h"
+
+namespace vigil {
+
+class FlowMod{
+public:
+    struct ofl_msg_flow_mod fm_msg;
+ 
+    /** Empty constructor
+    */
+    FlowMod();
+    
+    void
+    init_flow_mod();
+    
+    void
+    init_flow_mod(uint64_t cookie, uint64_t cookie_mask, uint8_t table_id,
+                   enum ofp_flow_mod_command cmd,uint16_t idlet, uint16_t hardt, 
+                   uint16_t prio, uint32_t buff_id, uint32_t out_port, 
+                   uint32_t out_group, uint16_t flags);
+    void
+    AddMatch(struct ofl_match *m);
+};
+
+}//namespace vigil
+
+#endif
+
+
